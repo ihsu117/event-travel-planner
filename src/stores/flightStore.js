cbee9ff5616c0b6cbd248ab7b31777ec40bff91f
ih6604 <ivanhsu117@gmail.com>
2025-02-06 15:20:58 -0500

    Flight Troubleshooting - Foreach

--- src/stores/flightStore.js
+++ src/stores/flightStore.js
@@ -21,8 +21,24 @@ export const useFlightStore = defineStore('flight', {
 
   actions: {
     setFlightResults(duffelFlights) {
-      this.flightResults = Object.keys(duffelFlights).map(flight => ({
-        flightDate: new Date(flight.departure_date),
+      // this.flightResults = duffelFlights.map(flight => ({
+      //   flightDate: new Date(flight.departure_date),
+      //   origin: flight.origin_airport,
+      //   destination: flight.destination_airport,
+      //   flightDepTime: flight.departure_time,
+      //   flightArrTime: flight.arrival_time,
+      //   seatNumber: '', // Not provided by Duffel search
+      //   seatAvailable: 1, // Default value
+      //   price: Math.round(flight.price),
+      //   flightType: 'Nonstop', // Can be determined by segments length
+      //   flightClass: 'Economy', // Default from search params
+      //   flightGate: flight.terminal || 'TBD',
+      //   airline: flight.airline
+      // }))
+
+      duffelFlights.forEach(flight => {
+        this.flightResults.push({
+          flightDate: new Date(flight.departure_date),
         origin: flight.origin_airport,
         destination: flight.destination_airport,
         flightDepTime: flight.departure_time,
@@ -34,7 +50,8 @@ export const useFlightStore = defineStore('flight', {
         flightClass: 'Economy', // Default from search params
         flightGate: flight.terminal || 'TBD',
         airline: flight.airline
-      }))
+        })
+      })
     },
 
     clearFlights() {    
