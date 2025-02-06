7cf8f2aa5ace3538cc717a392e8765901e74372c
Nicholas Chrysanthopoulos <ngc8500@rit.edu>
2025-02-05 19:18:44 -0500

    Login and Background Changes

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,51 @@
 <script setup>
+import { ref } from 'vue'
 import { PButton, PTextField } from '@poseidon-components'
 import '@poseidon-styles/index.css'
 
+const username = ref('')
+const password = ref('')
+const errors = ref({ username: '', password: '' })
+
+const validateForm = () => {
+  let isValid = true
+  errors.value = { username: '', password: '' }
+
+  if (!username.value) {
+    errors.value.username = 'Username is required'
+    isValid = false
+  }
+
+  if (!password.value) {
+    errors.value.password = 'Password is required'
+    isValid = false
+  } else if (password.value.length < 6) {
+    errors.value.password = 'Password must be at least 6 characters'
+    isValid = false
+  }
+
+  return isValid
+}
+
+const loginUser = async () => {
+  if (validateForm()) {
+    try {
+      // Here you would typically make an API call
+      console.log('Attempting login with:', {
+        username: username.value,
+        password: password.value
+      })
+      // await authService.login(username.value, password.value)
+    } catch (error) {
+      errors.value.password = 'Invalid username or password'
+    }
+  }
+}
 </script>
 
 <template>
   <div class="login-container">
-    <img src="@poseidon-assets/img/AppLogo.png" alt="Poseidon Logo" />
+    <img v-model="username" src="@poseidon-assets/img/AppLogo.png" alt="Poseidon Logo" />
     <div class="login-form">
       <div class="login-input">
         <PTextField design="p-textfield" label="Enter Username" />
@@ -16,7 +55,7 @@ import '@poseidon-styles/index.css'
         </div>
       </div>
       <div class="login-button">
-        <PButton design="login" label="Login">Submit</PButton>
+        <PButton @click="loginUser" design="login" label="Login">Submit</PButton>
         <p>Don't have an account? <a href="#">Sign up here!</a></p>
       </div>
     </div>
